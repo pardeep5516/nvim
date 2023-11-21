c4442933dfb26eee65b8cb72bec6c8b834f1ac37
pardeep yadav <pardeepyadav2914@gmail.com>
2023-11-21 19:12:01 +0530

    configure all basic settings and  lsp

---
+++ lua/plugins/lualine-nvim.lua
@@ -0,0 +1,43 @@
+local config = function()
+	local theme = require("lualine.themes.nightfox")
+
+	-- set bg transparency in all modes
+	theme.normal.c.bg = nil
+	-- theme.insert.c.bg = nil
+	-- theme.visual.c.bg = nil
+	-- theme.replace.c.bg = nil
+	-- theme.command.c.bg = nil
+
+	require("lualine").setup({
+		options = {
+			theme = theme,
+			globalstatus = true,
+		},
+		 sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+   inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+	})
+end
+
+return {
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	config = config,
+}
---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,25 @@
+local opts = {
+	ensure_installed = {
+		"efm",
+		-- "bashls",
+		-- "tsserver",
+		-- "solidity",
+		-- "tailwindcss",
+		-- "pyright",
+    -- "flake8",
+    -- "black",
+		"lua_ls",
+		-- "emmet_ls",
+		-- "jsonls",
+		-- "clangd",
+	},
+
+	automatic_installation = true,
+}
+
+return {
+	"williamboman/mason-lspconfig.nvim",
+	opts = opts,
+	event = "BufReadPre",
+	dependencies = "williamboman/mason.nvim",
+}
